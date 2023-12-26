e0ce8a984338d1c9b05737791d6c0561c8613729
Ahmed khaled <ahmedkh8907@gmail.com>
2023-12-26 21:49:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_login_ui/login_screen.dart';
+import 'package:flutter_responsive_login_ui/pallete.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login App',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Pallete.backgroundColor,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
